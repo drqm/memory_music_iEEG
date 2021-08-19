9d6103b81656c9cf5d024fe5b521ef2cc6483f13
drqm <dquiroga@clin.au.dk>
2021-08-19 15:59:32 +0200

    added tasks in Spanish

---
+++ scripts/LearningBachShort_iEEG_spanish.py
@@ -0,0 +1,197 @@
+
+"""
+Created on Fri Jul 17 16:59:51 2020
+
+@author: Leonardo Bonetti, Francesco Carlomagno
+
+leonardo.bonetti@clin.au.dk
+
+"""
+
+#To use this script please do the following:
+#-place the provided stimuli folders in your favorite directory
+#-assign to the variable 'stimidr' (few lines below) such directory
+#-define your log directory
+#Please note that the csv file with the crucial stimuli presentation IDs and 
+# behavioural responses is created within the log directory
+ 
+
+#################### YOUR DIRECTORY TO BE UPDATED BEFORE RUNNING THE SCRIPT ####################
+#THIS IS YOUR DIRECTORY WHERE YOU PUT THE PROVIDED FOLDERS, PLEASE UPDATE THIS, THANKS
+logdir = ('C:/Users/au571303/Documents/projects/memory_music_iEEG/logs') 
+stimdir = ('C:/Users/au571303/Documents/projects/memory_music_iEEG/stimuli')
+#################### Load libraries and set directories ####################
+from random import shuffle
+from psychopy import prefs
+#prefs.hardware['audioLib'] = ['pyo']
+prefs.hardware['audioLib'] = ['PTB']
+from psychopy import visual, core, sound, event, gui, monitors, logging
+import itertools as it
+import os
+import numpy as np
+
+#monitor settings
+#mon = monitors.Monitor('SonyG55')#fetch the most recent calib for this monitor
+#mon.setDistance(50)#further away than normal?
+#win = visual.Window(size=[1920,1080], monitor=mon)
+
+#################### Actual codes ####################
+
+##TEST
+
+# quit key
+def quitpd():
+    if logfile:
+       logfile.close()
+    core.quit()
+event.globalKeys.add(key='escape',func=quitpd,name='shutdown')
+
+#GUI for subject ID
+ID = gui.Dlg(title = 'subj ID')
+ID.addField('ID: ')
+subID = ID.show()
+
+#create csv file for log
+filename = logdir + '/' + subID[0] + '_learning_bach_iEEG_custom_spanish.csv'
+logfile = open(filename,'w')
+logfile.write("subj;trial;response;RT;time \n")
+
+#create log file
+block_time = core.Clock()
+logging.setDefaultClock(block_time)
+filename2 = logdir + '/' + subID[0] + '_learning_bach_iEEG_default_spanish.log'
+lastLog = logging.LogFile(filename2, level=logging.INFO, filemode='a')
+
+#getting file for learning phase
+os.chdir(stimdir + '/learningdef')
+Bachminor = sound.Sound('Bachminorprelude.wav')
+
+#getting files for recognition phase
+os.chdir(stimdir + '/recogdef')
+pathwave = stimdir + '/recogdef'
+wavefilesd = []
+wavenamesd = []
+for file in sorted(os.listdir(pathwave)):
+    if file.endswith(".wav"):
+        wavenamesd.append(file)
+        wavefilesd.append(sound.Sound(file))
+
+#reproducing several times (here 8 times) the 3 Old excerpts
+wnd = wavenamesd[24:] #extracting Old excerpts
+wnd = wnd * 8 #multiplying them by 8
+wfd = wavefilesd[24:]
+wfd = wfd * 8
+wavenames = wavenamesd[0:24] + wnd #getting the final list name
+wavefiles = wavefilesd[0:24] + wfd #getting the final list files
+wavezip = list(zip(wavenames,wavefiles))
+shuffle(wavezip)
+
+#initializing RT variable to get RTs
+RT = core.Clock()
+block_time = core.Clock() #this should not be useful anymore..
+
+#this is a bit redundant.. anyway..
+SPAAAAACE = 'space'
+
+#preparing window for the screen
+color_txt = 'white'
+win = visual.Window(fullscr = True, color = 'black')#[.9,.9,.9])
+#pd = visual.TextStim(win,text = 'First, you will listen to a complete musical piece '
+#                                '(Learning phase). \n\n Please try to remember it as ' 
+#                                'much as possible. \n\n Second, you will listen to 42 '
+#                                'short musical excerpts (Recognition phase). \n\n For '
+#                                'each of them you will be asked to indicate whether '
+#                                'they were extracted from the musical piece of the '
+#                                'learning phase or not. \n\n Press space to continue',
+#                                 color = color_txt, height = 0.1)
+pd = visual.TextStim(win,text = 'A continuación, usted escuchará una pieza musical completa repetida dos veces. '
+                                '\n\n Por favor, trate de memorizarla tanto como ' 
+                                'sea posible. \n\n Luego, usted realizará una prueba de memoria '
+                                'basada en esta música.\n\n Presione la barra espaciadora para empezar.',
+                                color = color_txt, height = 0.1)
+#instr = visual.TextStim(win,text = 'Learning phase \n\n Now you are going to listen '
+#                                   'to a complete musical piece \n\n Please try to '
+#                                   'remember it as much as possible \n\n Press space '
+#                                  'to continue',color = color_txt)
+instrrec = visual.TextStim(win,text = 'Reconocimiento..',color = color_txt)
+fix_c = visual.TextStim(win,text = '+', color = color_txt,height = 0.2)
+block_time.reset() #this should not be useful anymore..
+
+#actual experiment (learning phase)
+pd.draw()
+win.flip()
+event.waitKeys(keyList = [SPAAAAACE])
+#instr.draw()
+#win.flip()
+#event.waitKeys(keyList = [SPAAAAACE])
+ttime = block_time.getTime() #this should not be useful anymore..
+#playing the whole musical piece 2 times
+for ll in range(2):
+    dumm = 'Repetición ' + str(ll+1) + '\n Presione barra espaciadora para continuar.'
+    playlear = visual.TextStim(win,text = dumm, color = color_txt)
+    playlear.draw()
+    win.flip()
+    event.waitKeys(keyList = [SPAAAAACE])
+    fix_c.draw()
+    win.flip()
+    core.wait(0.5)
+    Bachminor.play() #open again this line later
+    event.clearEvents(eventType='keyboard')
+    core.wait(26) #open again this line later
+
+#actual experiment (recognition phase)
+playrec = visual.TextStim(win,text = 'Ahora usted escuchará 48 melodías cortas. \n\n '
+                                     'Para cada una de ellas, por favor indique con el teclado '
+                                     'si la memlodía se encontraba en la pieza musical '
+                                     'que usted acaba de oir (presione 1) '
+                                     'o no (presione 2). \n\n  Presione barra espaciadora para continuar.',
+                                     color = color_txt)
+playrec.draw()
+win.flip()
+event.waitKeys(keyList = [SPAAAAACE])
+#over musical excerpts (trials)
+for wavve in range(len(wavefiles)):
+    #displaying the progressive trial number for participants' comfort
+    jes = 'melodía número ' + str((wavve + 1)) + ' / ' + str(len(wavefiles))
+    instrrectr = visual.TextStim(win,text = jes,color = color_txt)
+    instrrectr.draw()
+    win.flip()
+    core.wait(1)
+    fix_c.draw()
+    win.flip()
+    core.wait(0.5)
+    ttime = block_time.getTime() #this should not be useful anymore..
+    wavezip[wavve][1].play()
+    event.clearEvents(eventType='keyboard')
+    RT.reset()
+    resp = None
+    while resp == None:
+        key = event.getKeys(keyList = ['1','2'])
+        if len(key) > 0:
+            rt = RT.getTime()
+            resp = key[0][0]
+        elif RT.getTime() > 3: #3 seconds of maximum wait if subject does not reply
+            resp = 0
+            rt = RT.getTime()
+    if RT.getTime() > 1.8:
+        core.wait(1)
+    else:
+        core.wait(1+1.8-RT.getTime())
+    #writing RT, trial ID, subject's response
+    lrow = '{};{};{};{};{} \n'
+    lrow = lrow.format(subID[0],wavezip[wavve][0], resp, round(rt*1000),str(ttime))
+    logfile.write(lrow)
+
+logging.flush()
+logfile.close()
+
+#final message
+playlear = visual.TextStim(win,text = 'Eso es todo! \n'
+                                      'Muchas gracias! \n\n '
+                                      'Presione barra espaciadora para finalizar esta parte del experimento.',
+                            color = color_txt)
+playlear.draw()
+win.flip()
+event.waitKeys(keyList = [SPAAAAACE])
+
+####################
---
+++ scripts/localizer_iEEG_spanish.py
@@ -0,0 +1,137 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sat Aug 22 16:11:11 2020
+
+@author: au571303
+"""
+from random import shuffle
+from psychopy import prefs
+prefs.hardware['audioLib'] = ['PTB']
+#prefs.hardware['audioLib'] = ['pyo']
+from psychopy import visual, core, sound, event, gui, logging
+import os
+import numpy as np
+#from triggers import setParallelData
+#setParallelData(0)
+
+# set your own project directory:
+os.chdir('C:/Users/au571303/Documents/projects/memory_music_iEEG')
+stim_dir = 'stimuli/manipulation_normalized'
+log_dir = 'logs'
+# uncomment to fixate randomization seed
+#rgnSeed = np.random.randint(900509)
+col = 'white'
+n_tones = 45 # 40 trials per tone for iEEG
+n_targets = 0
+
+# Set the frame rate of your screen. Not doing this may create timing issues
+frate = 60 #120
+prd = 1000/frate
+
+######################## create stimulus sequence ############################
+seq = np.zeros(n_tones*3 + n_targets)
+pattern = [1,2,3]
+
+tidx = np.random.choice(range(n_tones),n_targets, replace = False)
+tcount = 0
+end = 0
+p = 0
+for n in range(n_tones):
+    start = end
+    end = start + 3
+    if n > 0:
+        p = None
+    while p == None:
+        shuffle(pattern)
+        if pattern[0] != seq[start-1]:
+            p = 0
+    seq[start:end] = pattern 
+    if np.isin(n,tidx):
+        seq[end] = 4
+        end = end + 1 
+
+###################### Prepare psychopy task #################################
+
+####preload stimuli:
+sounds = [sound.Sound('{}/{}.wav'.format(stim_dir,int(s))) for s in np.unique(seq)]
+
+#### Prepare relevant keys:
+    
+#keyNext = 'space' # key to advance
+
+#### function to quit the experiment and save log file:
+def quit_and_save():
+    logging.flush()
+    core.quit()
+    
+event.globalKeys.add(key='escape', func=quit_and_save, name='shutdown')
+
+#### Collect participant identity:
+ID_box = gui.Dlg(title = 'Subject identity')
+ID_box.addField('ID: ')
+sub_id = ID_box.show()
+
+win = visual.Window(fullscr=True, color='black')
+
+##### create text oBjects to display during the experiment:
+
+instructions = visual.TextStim(win, text = "A continuación usted va a escuchar "
+                                           "una serie de sonidos. \n\n"
+                                           "Luego de que cada sonido termine, usted verá "
+                                           "la palabra 'IMAGINE' en la pantalla. \n\n"
+                                           "Cuando esto suceda, por favor reproduzca en su mente "
+                                           "e imagine vívidamente el sonido escuchado. \n\n"
+                                           "Presione la barra espaciadora para empezar.",
+                                         wrapWidth=1.8, color = col)
+                                         
+endText = visual.TextStim(win, text='Este es el final de la prueba. \n'
+                                    'Presione una tecla para terminar.',
+                          wrapWidth=1.8, color = col)
+
+count_txt = ['Escuche','Escuche','Imagine','Imagine']
+#durs = [550,600,550,600]
+durs = [400,400,400,400]
+fixationCross = visual.TextStim(win, text='+', color=col, height=0.2)
+
+# create a silent sound to prevent buffer issues
+silentDur = .5
+silent = sound.Sound('C', secs=silentDur, volume=0, sampleRate = 44100, stereo = False)
+
+# start log file:
+filename = log_dir + '/' + sub_id[0] + '_localizer_iEEG_spanish.log'
+lastLog = logging.LogFile(filename, level=logging.INFO, filemode='w')
+##############################################################################
+
+############################# Start the task #################################
+instructions.draw()
+win.flip()
+event.waitKeys()
+
+fixationCross.draw()
+win.flip()
+silent.play()
+core.wait(0.5)
+nextFlip = win.getFutureFlipTime(clock='ptb')
+
+for s in seq:
+#   win.callOnFlip(setParallelData, int(s))
+    win.callOnFlip(print, int(s))
+    sounds[int(s)-1].play(when = nextFlip)
+    for cidx,cc in enumerate(count_txt):
+        count_msg = visual.TextStim(win, text=cc, color = col, height = 0.2)
+        for frs in range(int(np.round(50/prd))): # 6 frames = 50 ms
+            count_msg.draw()
+            win.flip()
+    #            win.callOnFlip(setParallelData, 0)
+        win.callOnFlip(print, 0)
+        for frs in range(int(np.round(durs[cidx]/prd))): # 30 frames = 450 ms
+            count_msg.draw()
+            win.flip()
+    nextFlip = win.getFutureFlipTime(clock='ptb')
+    logging.flush()
+
+endText.draw()
+win.flip()
+event.waitKeys()
+win.close()
+core.quit() 
